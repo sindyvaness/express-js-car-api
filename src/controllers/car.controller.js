c54a4d9d8a469739558d9427d216be0b84c85b9c
sindyvaness <42687025+sindyvaness@users.noreply.github.com>
2025-11-18 15:17:00 -0800

    Creating the getCarsByBrand endpoint.

--- src/controllers/car.controller.js
+++ src/controllers/car.controller.js
@@ -14,3 +14,26 @@ export const getUniqueBrands = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const getCarsByBrand = async (req, res) => {
+  try {
+    let { brand } = req.params;
+
+    if (brand) {
+        brand = brand.toUpperCase();
+        const cars = await Car.find({ brand });
+
+        if (cars.length > 0) {
+            res.status(200).json(cars);
+        } else {
+            res.status(404).json({ message: "No cars found for this brand" });
+        }
+    } else {
+        res.status(400).json({ message: "Provide a brand" });
+    }
+    
+  } catch (error) {
+    console.error("Error buscando por brand:", error);
+    res.status(500).json({ message: "Error interno del servidor" });
+  }
+};
--- src/routes/car.routes.js
+++ src/routes/car.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getCars, getUniqueBrands } from "../controllers/car.controller.js";
+import { getCars, getUniqueBrands, getCarsByBrand } from "../controllers/car.controller.js";
 
 const router = express.Router();
 
 router.get("/", getCars);
 router.get("/brands", getUniqueBrands);
+router.get("/bybrand/:brand", getCarsByBrand)
 
 export default router;
